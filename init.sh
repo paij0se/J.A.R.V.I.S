3427fcb2e1d855a7c9987b17fb1e88b0a2240916
Jose Mejia <156923829+drpaij0se@users.noreply.github.com>
2024-03-18 16:35:25 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Downloading"
 git clone https://github.com/drpaij0se/J.A.R.V.I.S.
-cd J.A.R.V.I.S
+cd "J.A.R.V.I.S."
 echo "Installing"
 go get .
 echo "compiling"
-go build main.go
-echo "Done!"
+go build -o jarvis main.go
+echo "Done!, run ./J.A.R.V.I.S./jarvis"
